f435ca4550f6a1ecd781d6acd0fa545d254f2f47
Priyangshu <shomepriyangshu16@gmail.com>
2025-03-01 21:23:46 +0530

    init analytics..

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,41 @@
 // frontend/src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/Auth_Context";
+import { AuthContext, AuthProvider } from "./context/Auth_Context";
 import { ChatProvider } from "./context/Chat_Context";
+import { useContext, useEffect } from "react";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ChatPage from "./pages/ChatPage";
 import AdminDashboard from "./pages/AdminDashboard";
+import LogRocket from 'logrocket';
+import posthog from "posthog-js";
+
+
+LogRocket.init('tzrop0/unifyed');
 
 const App = () => {
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      LogRocket.identify(user._id, {
+        name: user.username,
+        email: user.email,
+      });
+    }
+  }, [user]); 
+
+  useEffect(() => {
+    if (user) {
+      posthog.identify(user._id, {
+        name: user.username,
+        email: user.email,
+      });
+
+      posthog.capture("User Logged In", { userId: user._id });
+    }
+  }, [user]);
+
   return (
     <AuthProvider>
       <ChatProvider>
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById('root')).render(
+const options = {
+  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  autocapture: true,
+  disable_session_recording: process.env.NODE_ENV === "development", 
+};
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
